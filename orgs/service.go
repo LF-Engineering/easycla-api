2ad6b56e4cceca9c3fa49a91950febf5c7023458
prasanna.mahajan <mahajanprasannap@gmail.com>
2019-10-25 20:24:19 +0530

    Adding orgs package implementing service,repository and handler
    Signed-off-by: prasanna.mahajan <mahajanprasannap@gmail.com>

---
+++ orgs/service.go
@@ -0,0 +1,24 @@
+package orgs
+
+import (
+	"context"
+
+	"github.com/communitybridge/easycla-api/gen/models"
+	"github.com/communitybridge/easycla-api/gen/restapi/operations/organization"
+)
+
+type Service interface {
+	GetOrgFoundations(ctx context.Context, in *organization.GetOrgFoundationsParams) (models.Organization, error)
+}
+
+type service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) Service {
+	return &service{repo}
+}
+
+func (s *service) GetOrgFoundations(ctx context.Context, in *organization.GetOrgFoundationsParams) (models.Organization, error) {
+	return s.repo.GetOrgFoundations(ctx, in)
+}
