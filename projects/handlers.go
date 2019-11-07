bf1190cfdec9e94f0afd51e296cee002b9ca808f
prasanna.mahajan <mahajanprasannap@gmail.com>
2019-11-07 15:19:49 +0530

    Implementing getProject api
    Signed-off-by: prasanna.mahajan <mahajanprasannap@gmail.com>

---
+++ projects/handlers.go
@@ -0,0 +1,38 @@
+package projects
+
+import (
+	"github.com/communitybridge/easycla-api/gen/models"
+	"github.com/communitybridge/easycla-api/gen/restapi/operations"
+	"github.com/communitybridge/easycla-api/gen/restapi/operations/project"
+	"github.com/go-openapi/runtime/middleware"
+)
+
+// Configure setups handlers on api with service
+func Configure(api *operations.ClaAPI, service Service) {
+	api.ProjectGetProjectHandler = project.GetProjectHandlerFunc(
+		func(params project.GetProjectParams) middleware.Responder {
+			result, err := service.GetProject(params.HTTPRequest.Context(), &params)
+			if err != nil {
+				return project.NewGetProjectBadRequest().WithPayload(errorResponse(err))
+			}
+			return project.NewGetProjectOK().WithPayload(result)
+		})
+}
+
+type codedResponse interface {
+	Code() string
+}
+
+func errorResponse(err error) *models.ErrorResponse {
+	code := ""
+	if e, ok := err.(codedResponse); ok {
+		code = e.Code()
+	}
+
+	e := models.ErrorResponse{
+		Code:    code,
+		Message: err.Error(),
+	}
+
+	return &e
+}
---
+++ projects/repository.go
@@ -0,0 +1,28 @@
+package projects
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/communitybridge/easycla-api/gen/models"
+)
+
+// Repository interface defines methods of project repository service
+type Repository interface {
+	GetProject(ctx context.Context, projectID string) (*models.Project, error)
+}
+
+type repository struct {
+}
+
+// NewRepository creates new instance of project repository
+func NewRepository() Repository {
+	return &repository{}
+}
+
+func (r *repository) GetProject(ctx context.Context, projectID string) (*models.Project, error) {
+	var p models.Project
+	compactProject := `{"projectID":"1234566....","projectName":"CFF Migration","templates":[{"type":"icla","legalEntityName":"","author":"some user lfid","url":"https://....","version":"v1","created":"iso datetime","updated":"iso datetime"},{"type":"ccla","legalEntityName":"","author":"some user lfid","url":"https://....","version":"v1","created":"iso datetime","updated":"iso datetime"}],"cclaEnabled":true,"iclaEnabled":true,"iclaSignatureRequired":true,"cclaCompaniesSigned":["companyID1","companyID2","companyID3","companyID4","companyID5","companyID6","companyID7","companyID8","companyID9","companyID10","companyID11","companyID12"],"whitelist":{"users":[{"type":"user","userID":"<user id 1>","signed":true,"signed_on":"iso datetime","last_activity":"iso datetime"},{"type":"user","userID":"<user id 2>","signed":true,"signed_on":"iso datetime","last_activity":"iso datetime"},{"type":"user","userID":"<user id 3>","signed":false,"signed_on":null,"last_activity":null}],"domains":[{"type":"domain","domain":"<domain string>"}],"emails":[{"type":"email","email":"<email string/regex>"}],"githubUsers":[{"type":"githubUser","githubUser":"<github user id"}],"githubOrgs":[{"type":"githubOrg","githubOrg":"<github org name/id"}]}}`
+	err := json.Unmarshal([]byte(compactProject), &p)
+	return &p, err
+}
