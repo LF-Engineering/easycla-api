95f5d7899a9d48d00e32ed00f0fdc26d2857299f
prasanna.mahajan <mahajanprasannap@gmail.com>
2019-11-18 15:13:04 +0530

    Adding database definition of cla_groups table
    Signed-off-by: prasanna.mahajan <mahajanprasannap@gmail.com>

---
+++ .build/db/migrations/20191118083102_create_cla_groups.sql
@@ -0,0 +1,22 @@
+-- migrate:up
+
+create table cla.cla_groups (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  cla_group_name varchar(255) UNIQUE NOT NULL,
+  foundation_id varchar(255) NOT NULL,
+  created_at BIGINT NOT NULL DEFAULT extract(epoch from now()),
+  updated_at BIGINT NOT NULL DEFAULT extract(epoch from now()),
+  ccla_enabled boolean,
+  icla_enabled boolean,
+  PRIMARY KEY(id)
+);
+
+create table cla.cla_group_project_managers (
+  cla_group_id uuid NOT NULL REFERENCES cla.cla_groups(id), 
+  project_manager_id uuid NOT NULL,
+  PRIMARY KEY(cla_group_id,project_manager_id)
+)
+-- migrate:down
+drop table cla.cla_group_project_managers;
+drop table cla.cla_groups;
+
